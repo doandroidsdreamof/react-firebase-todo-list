1144421f484bf374e4472631373756750b18b74d
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-20 13:34:00 +0300

    eslint-prettier config

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react'
 import React from 'react'
 
-function reducer(state, action) {
-  switch (action) {
-    case 'artış':
-      return state = true ;
-      break;
-    case 'azalış':
-      return state = false;
-      break;
-    default:
-      throw new Error()
-  }
-
-
-
-}
 
 
 
 function App() {
-  const [count, dispatch] = React.useReducer(reducer, 0)
+
+  function deneme(){
+    const x  = 1;
+  }
+console.log('deneme')
+
   return (
-    <div className="App">
-      <button onClick={() => dispatch('artış')}>arttır</button>
-      <button onClick={() => dispatch('azalış')}>azalt</button>
-      <h1>{count ? 'true' : 'false'}</h1>
+    <div className="">
+      <h1 className='text-8xl underline'>test</h1>
 
 
     </div>
