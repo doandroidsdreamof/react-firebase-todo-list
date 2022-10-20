5314a0c08555888cd860ae371ef8285704e946d7
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-20 13:10:06 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import React from 'react'
+
+function reducer(state, action) {
+  switch (action) {
+    case 'artış':
+      return state = true ;
+      break;
+    case 'azalış':
+      return state = false;
+      break;
+    default:
+      throw new Error()
+  }
+
+
+
+}
+
+
 
 function App() {
+  const [count, dispatch] = React.useReducer(reducer, 0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dispatch('artış')}>arttır</button>
+      <button onClick={() => dispatch('azalış')}>azalt</button>
+      <h1>{count ? 'true' : 'false'}</h1>
+
+
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAb96gU-AJGG-UmEwcBNTU1KS5hUuI3eek",
+  authDomain: "todo-list-7d913.firebaseapp.com",
+  projectId: "todo-list-7d913",
+  storageBucket: "todo-list-7d913.appspot.com",
+  messagingSenderId: "134956116629",
+  appId: "1:134956116629:web:3cb467357b7d3c2285499c"
+};
+
+const app = initializeApp(firebaseConfig);
