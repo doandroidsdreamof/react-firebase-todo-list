94808fd899cdf0ea5c8dc32b0e2c0bc183a3e711
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-21 12:38:16 +0300

    component type

--- src/layouts/FormWrapper.tsx
+++ src/layouts/FormWrapper.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 
-
-
-
- const FormWrapper = ({children}:any) => {
+const FormWrapper = ({ children }: any) => {
   return (
     <div className=' h-screen w-screen border-2 border-black  overflow-hidden flex justify-center bg-gray-200'>
-{children}
-     
-    
+      {children}
     </div>
   )
 }
 
-
-export default FormWrapper;
+export default FormWrapper
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,FC } from 'react'
 
- const Home = () => {
+ const Home: FC  = () => {
 
  
 
