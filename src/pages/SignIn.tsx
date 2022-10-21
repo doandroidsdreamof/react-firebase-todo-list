94246ccf2cfe919eb01e62d862c12d277f02b339
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-22 01:13:38 +0300

    email validation

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -28,16 +28,15 @@ const SignIn: FC = () => {
 
   function validateEmail(value: string) {
     let error
-
+    const emailValidation = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
     if (!value) {
       error = 'Required'
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    } else if (emailValidation.test(value)) {
       error = 'Invalid email address'
       setAlertBoxMail(true)
-    } if(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) || value.length == 0){
+    }
+    if (emailValidation.test(value) || value.length == 0) {
       setAlertBoxMail(false)
-
-
     }
 
     return error
