0dce5ab720eb628f74c2050fe521036406bbdfa1
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-11-03 02:53:29 +0300

    memoization todos

--- src/layouts/TodoWrapper.tsx
+++ src/layouts/TodoWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { FC, FormEvent, MouseEvent,useContext, useEffect, useState } from 'react'
+import React, { FC, FormEvent, MouseEvent, useContext, useEffect, useMemo, useState } from 'react'
 import { getAuth } from 'firebase/auth'
 import {
   addDoc,
@@ -19,25 +19,47 @@ import TodoListBlocks from '../components/todo/TodoListBlocks'
 import TopNavBar from '../components/todo/TopNavBar'
 import { AuthContext } from '../context/AuthContext'
 import { db } from '../firebase'
-import { singleTodo,TodoWrapperChildren } from '../types/Todos'
+import { singleTodo, TodoWrapperChildren } from '../types/Todos'
 
 const TodoWrapper: FC<TodoWrapperChildren> = (props) => {
   const auth = getAuth()
   const user = useContext(AuthContext)
   const [modal, setModal] = useState<boolean>(false)
-  const [singleTodo, passSingleTodo] = useState<singleTodo>({todo: '',id: ''})
-
+  const [singleTodo, passSingleTodo] = useState<singleTodo>({ todo: '', id: '' })
 
   const handleSingleTodo = async (e) => {
-    passSingleTodo({todo:e.todo, id: e.id})
+    passSingleTodo({ todo: e.todo, id: e.id })
   }
 
   const deleteTodo = async (id) => {
     await deleteDoc(doc(db, 'Todo', id))
     props.logic(false)
-
   }
 
+  const memeTodos = React.useMemo(
+    () =>
+      props.TodosData?.map((todos) => {
+        return (
+          <>
+            <TodoListBlocks>
+              <span
+                className='text-white inline font-medium text-ellipsis truncate  font-roboto p-4  w-ninty '
+                key={todos.id}
+              >
+                {todos.todo}
+              </span>
+              <button>
+                <EditButton />
+              </button>
+              <button onClick={(e) => handleSingleTodo(todos)}>
+                <DeleteButton openModal={(e) => setModal(e)} />
+              </button>
+            </TodoListBlocks>
+          </>
+        )
+      }),
+    [props.TodosData],
+  )
 
   return (
     <div
@@ -47,31 +69,19 @@ const TodoWrapper: FC<TodoWrapperChildren> = (props) => {
           : ' px-5 md:px-20 lg:px-48 absolute w-onehundred overflow-hidden bg-bg-color min-h-screen  '
       }
     >
-      <TopNavBar/>
+      <TopNavBar />
       <div className='flex flex-col  justify-center   items-center gap-y-8'>
         {props.TodoHead}
         <div className=' w-onehundred justify-center flex  flex-col overflow-x-hidden  '>
           {props.TextInput}
-          <ModalDelete deleteTodo={(e) => deleteTodo(e)}  singleTodo={singleTodo} open={modal} closeModal={(e) => setModal(e)} />
+          <ModalDelete
+            deleteTodo={(e) => deleteTodo(e)}
+            singleTodo={singleTodo}
+            open={modal}
+            closeModal={(e) => setModal(e)}
+          />
           <div className='flex flex-col-reverse items-center mb-4  w-onehundred justify-center'>
-            {props.TodosData?.map((todos, index) => (
-              <>
-                <TodoListBlocks>
-                  <span
-                    className='text-white inline font-medium text-ellipsis truncate  font-roboto p-4  w-ninty '
-                    key={index}
-                  >
-                    {/* {todos.todo} */}
-                  </span>
-                  <button>
-                    <EditButton />
-                  </button>
-                  <button onClick={(e) => handleSingleTodo(todos)}>
-                    <DeleteButton openModal={(e) => setModal(e)} />
-                  </button>
-                </TodoListBlocks>
-              </>
-            ))}
+            {memeTodos}
           </div>
         </div>
       </div>
