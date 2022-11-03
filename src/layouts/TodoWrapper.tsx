13a694253cf1a294c803d0d696c61db01d8a3128
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-11-03 09:20:57 +0300

    line-through

--- src/layouts/TodoWrapper.tsx
+++ src/layouts/TodoWrapper.tsx
@@ -28,11 +28,12 @@ const TodoWrapper: FC<TodoWrapperChildren> = (props) => {
   const [modal, setModal] = useState<boolean>(false)
   const [editModal, setEditModal] = useState<boolean>(false)
   const [update, watchUpdate] = useState<boolean>(false)
+  const [completed, setCompleted] = useState({})
   const [singleTodo, passSingleTodo] = useState<singleTodo>({ todo: '', id: '' })
 
   useEffect(() => {
     props.logic()
-  }, [update])
+  }, [update, completed])
 
   const handleSingleTodo = async (e) => {
     await passSingleTodo({ todo: e.todo, id: e.id })
@@ -44,27 +45,42 @@ const TodoWrapper: FC<TodoWrapperChildren> = (props) => {
     props.logic()
   }
 
+  const handleSelect = (dataName) => {
+  
+    
+  }
+
+  const todoTextStyle =
+    'text-white inline  font-medium cursor-pointer text-ellipsis truncate  font-roboto p-4  w-ninty'
+
   const memeTodos = React.useMemo(
     () =>
-      props.TodosData?.map((todos) => {
+      props.TodosData?.map((todos,index) => {
         return (
           <>
             <TodoListBlocks>
               <span
-                className='text-white inline font-medium text-ellipsis truncate  font-roboto p-4  w-ninty '
-                key={todos.id}
+                onClick={(e) => setCompleted(!completed)}
+                className={completed ? `line-through ${todoTextStyle}` : ` ${todoTextStyle}`}
+                key={index}
               >
                 {todos.todo}
               </span>
-              <div className=' h-full  flex m-auto flex-row gap-x-1' >
-                <EditButton onClick={(e) => handleSingleTodo(todos)} editModal={(e) => setEditModal(e)} />
-                <DeleteButton onClick={(e) => handleSingleTodo(todos)} openModal={(e) => setModal(e)} />
+              <div className=' h-full  flex m-auto flex-row gap-x-1'>
+                <EditButton
+                  onClick={(e) => handleSingleTodo(todos)}
+                  editModal={(e) => setEditModal(e)}
+                />
+                <DeleteButton
+                  onClick={(e) => handleSingleTodo(todos)}
+                  openModal={(e) => setModal(e)}
+                />
               </div>
             </TodoListBlocks>
           </>
         )
       }),
-    [props.TodosData],
+    [props.TodosData,completed],
   )
 
   return (
