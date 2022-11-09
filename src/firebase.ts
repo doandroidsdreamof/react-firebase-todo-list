0ef370d461bc24086be5b78d14af7aae0780cbfc
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-11-09 06:16:26 +0300

    .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,16 +6,16 @@
 
 
   const firebaseConfig = {
-  apiKey: "AIzaSyAb96gU-AJGG-UmEwcBNTU1KS5hUuI3eek",
-  authDomain: "todo-list-7d913.firebaseapp.com",
-  projectId: "todo-list-7d913",
-  storageBucket: "todo-list-7d913.appspot.com",
-  messagingSenderId: "134956116629",
-  appId: "1:134956116629:web:3cb467357b7d3c2285499c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-// export const firestore = getFirestore(app);
+
